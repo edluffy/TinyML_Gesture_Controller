2cc636d0e353a174bf5e40313dc8866ab42253c9
edluffy <edward@ncade.com>
2021-01-07 22:02:49 +0000

    Add script to collect accelerometer data

--- data_collect.ino
+++ data_collect.ino
@@ -1,38 +1,57 @@
-#include <PDM.h>
+#include <Arduino_LSM9DS1.h>
 
-short sample_buf[256];
-
-volatile int sample_cnt;
+char wake_key = 'A';
 
 void setup()
 {
+	// Initialise Serial communication
 	Serial.begin(9600);
 	while(!Serial);
 	Serial.println("Started serial communication");
 
-	PDM.onReceive(on_pdm);
-
-	// 1 channel, 16kHz sample rate
-	if (!PDM.begin(1, 16000)){
-		Serial.println("Failed to start PDM!");
+	// Initialise IMU
+	if(!IMU.begin()){
+		Serial.println("Failed to start IMU!");
 		while(1);
 	}
+
+	pinMode(LED_BUILTIN, OUTPUT);
+
+	// Serial.print("Accelerometer sample rate = ");
+	// Serial.print(IMU.accelerationSampleRate());
+	// Serial.println(" Hz");
+
+	Serial.println("X\tY\tZ");
 }
 
 void loop()
 {
-	if(sample_cnt){
-		for(int i=0; i < sample_cnt; i++){
-			Serial.println(sample_buf[i]);
-		}
-		sample_cnt = 0;
+	if(Serial.available()){
+		char input = Serial.read();
+		if(input == wake_key)
+			read_imu();
 	}
 }
 
 
-void on_pdm()
+// Record 100 samples for approx. 4 sec
+void read_imu()
 {
-	int bytes_avail = PDM.available();
-	PDM.read(sample_buf, bytes_avail);
-	sample_cnt = bytes_avail / 2;
+	float x, y, z;
+	digitalWrite(LED_BUILTIN, HIGH);
+	Serial.print("\n\n\n\n\n\n");
+	Serial.println("-, -, -");
+
+	for(int i=0; i < 100; i++){
+		if(IMU.accelerationAvailable()){
+			IMU.readAcceleration(x, y, z);
+			Serial.print(x);
+			Serial.print('\t');
+			Serial.print(y);
+			Serial.print('\t');
+			Serial.println(z);
+			delay(40);
+		}
+	}
+	digitalWrite(LED_BUILTIN, LOW);
 }
